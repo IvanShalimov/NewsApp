db1fc7a0bf20e349c276c6046b821ea619fe9a86
ivanshalimov <IShalimov@alfabank.ru>
2022-10-11 23:03:16 +0300

    Try make list in compose tutor

--- app/src/main/java/com/example/newsapp1/presentation/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp1/presentation/NewsActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -25,6 +30,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.newsapp1.R
 import com.example.newsapp1.presentation.ui.theme.NewsApp1Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class NewsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +41,7 @@ class NewsActivity : ComponentActivity() {
         setContent {
             NewsApp1Theme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -50,7 +59,14 @@ class NewsActivity : ComponentActivity() {
                     .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            )
+
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     message.name,
                     color = MaterialTheme.colors.secondary,
@@ -58,10 +74,14 @@ class NewsActivity : ComponentActivity() {
                 )
                 Spacer(modifier = Modifier.width(4.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium) {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)) {
                     Text(
                         message.body,
-                        modifier = Modifier.padding(4.dp),
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if(isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.body1
                     )
                 }
@@ -71,7 +91,16 @@ class NewsActivity : ComponentActivity() {
 
     }
 
-    @Preview(name = "Light Mode")
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message = message)
+            }
+        }
+    }
+
+   /* @Preview(name = "Light Mode")
     @Preview(
         uiMode = Configuration.UI_MODE_NIGHT_YES,
         showBackground = true,
@@ -82,6 +111,21 @@ class NewsActivity : ComponentActivity() {
         NewsApp1Theme {
             Surface(modifier = Modifier.fillMaxSize()) {
                 MessageCard(Message("Android", "Jetpack Compose"))
+            }
+        }
+    }*/
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewConversation() {
+        NewsApp1Theme {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Conversation(SampleData.conversationSample)
             }
         }
     }
